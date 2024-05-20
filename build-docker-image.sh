fe1f6f2c9e06f26db5b3f5239d418e4abd8c9aeb
Smruti Padhy <spadhy@tacc.utexas.edu>
2024-05-20 11:53:18 -0400

    build image tag updated

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -6,10 +6,13 @@ echo "###### start build.sh #####"
 cd ./Docker
 pwd
 
-export RHVERSION=4.1.6
+#export RHVERSION=4.1.6-dev-aggrs
+#export RHVERSION=dev-vdj
+export RHVERSION=dev
+
 
 echo "###### Building Docker image for MetaV3 RH Server Core Version "$RHVERSION
-docker build -t tapis/tapis-meta-rh-core:$RHVERSION .
+docker build -t tapis/tapis-meta-rh-server:$RHVERSION .
 
-docker push tapis/tapis-meta-rh-core:$RHVERSION
+#docker push tapis/tapis-meta-rh-core:$RHVERSION
 echo "###### end build.sh #####"
